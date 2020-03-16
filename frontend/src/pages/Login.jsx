727c96b5fd8d628ed4fc5a962a60cdead8d95971
Rizvi <rizvida1@gmail.com>
2020-03-16 17:41:12 -0400

    Slight Modifications to login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -29,63 +29,67 @@ const Login = () => {
         style={{ minHeight: "100vh" }}
       >
         <Card>
-          <Row type="flex" justify="center" align="middle">
+          <Row type="flex" justify="space-around" align="middle">
             <Col xs={24} lg={11}>
               <Row type="flex" justify="center" align="middle">
                 <Title>Welcome to [INSERT GENERIC NAME HERE]</Title>
               </Row>
             </Col>
             <Col xs={0} lg={2}>
-              <Divider type="vertical" style={{ height: "140px" }} />
+              <Row type="flex" justify="space-around" align="middle">
+                <Divider type="vertical" style={{ height: "140px" }} />
+              </Row>
             </Col>
             <Col xs={24} lg={11}>
-              <Form
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                style={{ width: "500px" }}
-              >
-                <Form.Item
-                  name="username"
-                  rules={[
-                    { required: true, message: "Please input your Username!" }
-                  ]}
+              <Row type="flex" justify="space-around" align="middle">
+                <Form
+                  initialValues={{ remember: true }}
+                  onFinish={onFinish}
+                  style={{ width: "400px" }}
                 >
-                  <Input
-                    prefix={<UserOutlined className="site-form-item-icon" />}
-                    placeholder="Username"
-                  />
-                </Form.Item>
-                <Form.Item
-                  name="password"
-                  rules={[
-                    { required: true, message: "Please input your Password!" }
-                  ]}
-                >
-                  <Input
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                  />
-                </Form.Item>
-                <Form.Item>
-                  <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
+                  <Form.Item
+                    name="username"
+                    rules={[
+                      { required: true, message: "Please input your Username!" }
+                    ]}
+                  >
+                    <Input
+                      prefix={<UserOutlined className="site-form-item-icon" />}
+                      placeholder="Username"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      { required: true, message: "Please input your Password!" }
+                    ]}
+                  >
+                    <Input
+                      prefix={<LockOutlined className="site-form-item-icon" />}
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </Form.Item>
+                  <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                      <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <a style={{ float: "right" }}>Forgot password</a>
                   </Form.Item>
 
-                  <a style={{ float: "right" }}>Forgot password</a>
-                </Form.Item>
-
-                <Form.Item>
-                  <Button type="primary" htmlType="submit" block>
-                    Log in
-                  </Button>
-                  {/* Or <a href="">register now!</a> */}
-                </Form.Item>
-              </Form>
+                  <Form.Item style={{ margin: 0 }}>
+                    <Button type="primary" htmlType="submit" block>
+                      Log in
+                    </Button>
+                    {/* Or <a href="">register now!</a> */}
+                  </Form.Item>
+                </Form>
+              </Row>
             </Col>
           </Row>
         </Card>
-      </Row>{" "}
+      </Row>
     </div>
   );
 };
